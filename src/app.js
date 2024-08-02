0670fa5ea5a1b13ea10bf16c5779b6a6452f2018
Ritik verma <vermaritik933@gmail.com>
2024-08-02 20:46:32 +0530

    Create Routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const ownersRouter = require('./routes/ownersRouter');
+const usersRouter = require('./routes/usersRouter');
+const productsRouter = require('./routes/productsRouter');
 
 
 app.use(express.json({
@@ -9,9 +12,17 @@ app.use(express.json({
 }));
 
 app.use(express.urlencoded({
-    extended: true, limit: "16kb"
-}))
+    extended: true, 
+    limit: "16kb"
+}));
+
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "ejs");
+
+app.use("/owners" , ownersRouter);
+app.use("/users" , usersRouter);
+app.use("/products" , productsRouter);
+
 module.exports = app;
